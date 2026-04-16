c33a553d4c65db0a7bd605c372d38adf6f849bd4
Ruggeri-Agustin <agus.ruggeri07@gmail.com>
2026-04-16 10:56:21 -0300

    cambios 16/04/2026

--- src/app/compartidos/nav/nav.ts
+++ src/app/compartidos/nav/nav.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Productos } from '../../paginas/productos/productos';
+import { Ofertas } from '../../paginas/ofertas/ofertas';
 
 @Component({
   selector: 'app-nav',
