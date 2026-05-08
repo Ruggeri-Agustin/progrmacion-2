ffd6806c259fe2fbe164034398b2c001002d8ca7
Ruggeri-Agustin <agus.ruggeri07@gmail.com>
2026-05-08 10:56:52 -0300

    cambios 08/05/2026

---
+++ src/app/models/producto.ts
@@ -0,0 +1,13 @@
+export interface Producto {
+  id: number;
+  img: string;
+  nombre: string;
+  desarrolladora: string;
+  precio: number;
+  desc: string;
+  cr1: string;
+  cr2: string;
+  cr3: string;
+  cr4: string;
+  disponibilidad: boolean;
+}
